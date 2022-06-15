27976b48b0dd1e3931e08e3122601fe61615a5e4
josevitor <josevitoralves561@gmail.com>
2022-06-15 20:53:13 -0300

    added card sorting and turning

--- app.js
+++ app.js
@@ -1,16 +1,55 @@
-
+let counter = 0;
 let N_card = 1;
+let arr = ["ross","explody","fiesta","metal","triplets","unicorn","revertit"]
+const table = document.querySelector(".table_desktop");
+
 while(N_card%2 != 0){
     N_card = prompt("Insira o numero de cartas para jogar:")
 }
 
-const table = document.querySelector(".table_desktop")
+for (let i = 0 ; i< N_card ; i++){
+    addCard();
+}
+//sorts the card array
+sortCard(arr);
+arr = arr.slice(0,(N_card/2))   
+arr = arr.concat(arr)
+sortCard(arr);
+addSortedCards();
 
-let counter = 0;
+function addSortedCards(){
+    let cardList = document.querySelectorAll(".card .card_back")
+    const len = cardList.length
+    for(let i = 0;i<len;i++){
+        cardList[i].classList.add(arr[i])
+    }
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function sortCard(arr){
+    arr.sort(comparador);
+}
 
 
 function addCard(){
-    const card = table.createElement("div")
-    card.classList.add(card);
-    table.appendChild(card)    
+    const card=document.createElement("div");
+    const front_image = document.createElement("img");
+    const back_image = document.createElement("img");
+    front_image.classList.add("card_front")
+    back_image.classList.add("hidden","card_back")
+
+    card.classList.add("card");
+    table.appendChild(card);
+    card.appendChild(front_image);
+    card.appendChild(back_image);
+    card.setAttribute("onclick",'turnCard(this)')
 }
+
+function turnCard(element){
+    element.children[0].classList.toggle("hidden")
+    element.children[1].classList.toggle("hidden")
+}
+
