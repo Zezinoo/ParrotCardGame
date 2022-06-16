089348b7757537cbbb7d19e4b550d1925647f5ce
josevitor <josevitoralves561@gmail.com>
2022-06-16 20:55:11 -0300

    added game logic to finish and mark pairs

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let counter = 0;
+
+let play_counter = 0;
 let N_card = 1;
 let arr = ["ross","explody","fiesta","metal","triplets","unicorn","revertit"]
 const table = document.querySelector(".table_desktop");
 
-while(N_card%2 != 0){
+
+while(N_card%2 !==0 || N_card < 4 || N_card > 14){
+    console.log(N_card)
     N_card = prompt("Insira o numero de cartas para jogar:")
 }
 
@@ -13,15 +16,50 @@ for (let i = 0 ; i< N_card ; i++){
 //sorts the card array
 sortCard(arr);
 arr = arr.slice(0,(N_card/2))   
-arr = arr.concat(arr)
-sortCard(arr);
+doublearr = arr.concat(arr)
+sortCard(doublearr);
 addSortedCards();
 
+
+
+
+
+
+
+function checkMatch(){
+    if(play_counter%2===0 && play_counter!==0){
+        let turned_List = document.querySelectorAll(".turned:not(.found)")
+        const first_pair_image = turned_List[0].children[1].firstChild.classList[0]
+        const second_pair_image = turned_List[1].children[1].firstChild.classList[0]
+        if(first_pair_image===second_pair_image){
+            arr.splice(arr.indexOf(first_pair_image),1)
+            turned_List[0].classList.add("found")
+            turned_List[1].classList.add("found")
+        }
+        else{
+            setTimeout(() => {turn(turned_List[0]);}, 1000);
+            setTimeout(() => {turn(turned_List[1]);}, 1000);
+            setTimeout(() => {turned_List[0].setAttribute("onclick",'turnCard(this)');},1001)
+            setTimeout(() => {turned_List[1].setAttribute("onclick",'turnCard(this)');},1001)
+
+        }
+
+    }
+}
+
+function checkGameEnd(){
+    console.log("checked")
+    if (arr.length === 0){
+        setTimeout(() => {alert(`Você ganhou em ${play_counter} jogadas!`)},600)
+    }
+}
+
+
 function addSortedCards(){
-    let cardList = document.querySelectorAll(".card .card_back")
+    let cardList = document.querySelectorAll(".card .card_back img")
     const len = cardList.length
     for(let i = 0;i<len;i++){
-        cardList[i].classList.add(arr[i])
+           cardList[i].classList.add(doublearr[i])
     }
 }
 
@@ -33,25 +71,37 @@ function sortCard(arr){
     arr.sort(comparador);
 }
 
-
 function addCard(){
     const card=document.createElement("div");
-    const front_image = document.createElement("img");
-    const back_image = document.createElement("img");
-    front_image.classList.add("card_front")
-    back_image.classList.add("hidden","card_back")
+    const card_front = document.createElement("div");
+    const card_back = document.createElement("div");
+    const front_image = document.createElement("img")
+    const back_image = document.createElement("img")
+    
+    card_front.classList.add("card_front")
+    front_image.classList.add("front_image")
+    card_back.classList.add("card_back")
 
     card.classList.add("card");
     table.appendChild(card);
-    card.appendChild(front_image);
-    card.appendChild(back_image);
+    card.appendChild(card_front);
+    card.appendChild(card_back);
+    card_front.appendChild(front_image);
+    card_back.appendChild(back_image);
     card.setAttribute("onclick",'turnCard(this)')
 }
 
 function turnCard(element){
-    element.children[0].classList.toggle("hidden")
-    element.children[1].classList.toggle("hidden")
+    turn(element)
+    play_counter++ 
+    console.log(play_counter)
+    checkMatch();
+    console.log(arr)
+    checkGameEnd();
 }
 
-
+function turn(element){
+    element.classList.toggle("turned")
+    element.setAttribute("onclick","")
+}
 
